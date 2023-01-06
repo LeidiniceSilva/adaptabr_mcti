30eb85171faba85255e425e1086d06d4fad954af
Leidinice Silva <leidinicesilva@gmail.com>
2023-01-05 23:30:12 -0300

    update new scripts

--- comp_statist_indices.py
+++ comp_statist_indices.py
@@ -11,61 +11,30 @@ import scipy.stats as st
 from scipy.stats import norm
 
 
-def compute_corr(model, obs, **kwargs):
+def compute_corr(model, obs):
 
     """
 	The input arrays must have the same dimensions
-	:Param model: Numpy array with model data
-	:Param obs: Numpy array with obs data
-    Return: Pearson correlation
+	Param model: Numpy array with model data
+	Param obs: Numpy array with obs data
+    Return: Pearson Correlation Coefficient
     """
-
-    method = kwargs.pop('method', '3d')  # 1d ou 3d
-
-    if method == '1d':
-
-        corr, pvalue = st.pearsonr(model, obs)
-
-        return corr
-
-    elif method == '3d':
-
-        timelen = float(obs.shape[0])
-
-        obs_mean = np.nanmean(obs, axis=0)
-        obs_std = np.nanstd(obs, axis=0)
-
-        model_mean = np.nanmean(model, axis=0)
-        model_std = np.nanstd(model, axis=0)
-
-        x1 = (obs - obs_mean)/obs_std
-        y1 = (model - model_mean)/model_std
-
-        xymult = x1 * y1
-
-        xysum = np.nansum(xymult, axis=0)
-
-        corr = xysum/timelen
-
-        return corr
-
-    else:
-
-        print('ClimateStats.compute_pearson function')
-        print('Input data error')
-        exit(1)
-        
+    
+    corr, pvalue = st.pearsonr(obs, model)
+    
+    return corr, pvalue
+           
 
 def compute_r2(model, obs):
 
 	"""
 	The input arrays must have the same dimensions
-	:Param model: Numpy array with model data
-	:Param obs: Numpy array with obs data
-	:Return: R-squared
+	Param model: Numpy array with model data
+	Param obs: Numpy array with obs data
+	Return: R-squared
 	"""
 
-	corr = np.corrcoef(obs, model)[0][1]
+	corr, pvalue = st.pearsonr(obs, model)
 	r2 = corr ** 2
 
 	return r2
@@ -75,9 +44,9 @@ def compute_mae(model, obs):
 
     """
     The input arrays must have the same dimensions
-    :Param model: Numpy array with model data
-    :Param obs: Numpy array with obs data
-    :Return: Mean Absoluty Error
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Mean Absoluty Error
     """
 
     mae = np.mean(np.abs(np.array(model) - np.array(obs)))
@@ -89,9 +58,9 @@ def compute_rmse(model, obs):
 
     """
     The input arrays must have the same dimensions
-    :Param model: Numpy array with model data
-    :Param obs: Numpy array with obs data
-    :Return: Root Mean Square Error
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Root Mean Square Error
     """
 
     rmse = np.sqrt(((np.array(model) - np.array(obs)) ** 2).mean()) 
@@ -103,9 +72,9 @@ def compute_mbe(model, obs):
 
     """
     The input arrays must have the same dimensions
-    :Param model: Numpy array with model data
-    :Param obs: Numpy array with obs data
-    :Return: Mean Bias Error
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Mean Bias Error
     """
 
     mbe = np.nanmean(np.array(model) - np.array(obs))
@@ -117,9 +86,9 @@ def compute_pbias(model, obs):
 
     """
     The input arrays must have the same dimensions
-    :Param model: Numpy array with model data
-    :Param obs: Numpy array with obs data
-    :Return: Percentage Bias
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Percentage Bias Error
     """
 
     pbias = 100.0 * sum(np.array(model) - np.array(obs)) / sum(np.array(obs))
@@ -131,9 +100,9 @@ def compute_apb(model, obs):
 
     """
     The input arrays must have the same dimensions
-    :Param model: Numpy array with model data
-    :Param obs: Numpy array with obs data
-    :Return: Absolute Percent Bias
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Absolute Percent Bias Error
     """
 
     apb = 100.0 * sum(np.abs(np.array(model), np.array(obs))) / sum(np.array(obs))
@@ -145,9 +114,9 @@ def compute_nse(model, obs):
 
     """
     The input arrays must have the same dimensions
-    :Param model: Numpy array with model data
-    :Param obs: Numpy array with obs data
-    :Return: Nash–Sutcliffe Efficient Coefficient
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Nash–Sutcliffe Efficient Coefficient
     """
     
     p1 = sum((model - obs) ** 2)
@@ -161,9 +130,9 @@ def compute_diso(model, obs):
 
 	"""
 	The input arrays must have the same dimensions
-	:Param model: Numpy array with model data
-	:Param obs: Numpy array with obs data
-	:Return: Distance between Indices of Simulation and Observation
+	Param model: Numpy array with model data
+	Param obs: Numpy array with obs data
+	Return: Distance between Indices of Simulation and Observation
 	"""
 
 	p0 = np.abs(np.array(model).mean())
@@ -179,9 +148,9 @@ def compute_ioa(model, obs):
 
     """
     The input arrays must have the same dimensions
-    :Param model: Numpy array with model data
-    :Param obs: Numpy array with obs data
-    :Return: Index of Agreement
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Index of Agreement
     """
 
     p1 = (model - obs)**2
@@ -192,13 +161,13 @@ def compute_ioa(model, obs):
     return ioa
       
     
-def compute_ivs(obs, model):
+def compute_ivs(model, ob):
 
     """
     The input arrays must have the same dimensions
-    :Param obs: Numpy array with obs data
-    :Param model: Numpy array with model data
-    :Return: Interannual Variability Skill Score
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Interannual Variability Skill Score
     """
 
     p1 = np.std(obs)
@@ -209,13 +178,31 @@ def compute_ivs(obs, model):
     
     return ivs    
     
-    
+
+def compute_kge(model, ob):
+
+    """
+    The input arrays must have the same dimensions
+    Param model: Numpy array with model data
+    Param obs: Numpy array with obs data
+    Return: Kling-Gupta Efficiency
+    """
+
+    p1 = np.corrcoef(obs, model)[0][1]
+    p2 = np.nanmean(model) / np.nanmean(obs)
+    p3 = np.std(model, ddof=1) / np.mean(model) * 100 
+    p4 = np.std(obs, ddof=1) / np.mean(obs) * 100 
+    p5 = p3/p4
+    kge = 1 - np.sqrt((1 - p1)**2 + (1 - p2)**2 + (1 - p5)**2)
+
+	return kge
+	
+	
 def compute_cdf(dataset):
 
 	"""
-	The input arrays must have the same dimensions
-	:Param dataset: Numpy array with model or obs data
-	:Return: Cumulative Distribution Function
+	Param dataset: Numpy array with model or obs dataset
+	Return: Cumulative Distribution Function
 	"""
 
 	x = np.linspace(np.min(dataset), np.max(dataset))
@@ -229,8 +216,7 @@ def compute_cdf(dataset):
 def compute_pdf(dataset):
 
 	"""
-	The input arrays must have the same dimensions
-	:Param dataset: Numpy array with model or obs data
+	:Param dataset: Numpy array with model or obs dataset
 	:Return: Probability Density Function
 	"""
 
@@ -241,3 +227,4 @@ def compute_pdf(dataset):
 
 	return x, pdf
     
+
