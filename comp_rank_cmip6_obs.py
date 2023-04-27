69506563b93d6c49fc08f0aa74b4f23698d9efc6
Leidinice Silva <leidinicesilva@gmail.com>
2023-04-27 17:59:48 -0300

    update scripts

--- comp_rank_cmip6_obs.py
+++ comp_rank_cmip6_obs.py
@@ -10,11 +10,11 @@ import netCDF4
 import numpy as np
 
 from dict_cmip6_models_name import cmip6, cmip6_i
-from comp_statistical_metrics import compute_mbe
-from comp_statistical_metrics import compute_rmse
-from comp_statistical_metrics import compute_tss
-from comp_statistical_metrics import compute_pcc
-from comp_statistical_metrics import compute_ivs
+from comp_stats_metrics import compute_mbe
+from comp_stats_metrics import compute_rmse
+from comp_stats_metrics import compute_tss
+from comp_stats_metrics import compute_pcc
+from comp_stats_metrics import compute_ivs
 
 
 def import_obs_latlon(param, area, period, date):
@@ -68,8 +68,9 @@ def import_obs_ann(param, area, period, date):
 	lat   = data.variables['lat'][:]
 	lon   = data.variables['lon'][:]
 	value = var[:][:,:,:]
+	ts_ann = np.nanmean(np.nanmean(value, axis=1), axis=1)
 	
-	return value
+	return ts_ann
 	
 	
 def import_cmip_latlon(param, area, model, exp, period, date):
@@ -123,16 +124,21 @@ def import_cmip_ann(param, area, model, exp, period, date):
 	lat   = data.variables['lat'][:]
 	lon   = data.variables['lon'][:]
 	value = var[:][:,:,:]
-	
-	return value
+	ts_ann = np.nanmean(np.nanmean(value, axis=1), axis=1)
+
+	return ts_ann
 	
 
 def sort_list(data_list):
 	
 	li = []
 	for i in range(len(data_list)):
+		
+		if data_list[i] < 0:
+			data_list[i] = data_list[i]*(-1)
+			
 		li.append([data_list[i], i])
-	  
+	
 	li.sort()
 	sort_index = []
 	for x in li:
@@ -158,7 +164,7 @@ def sort_list_reverse(data_list):
 	li = []
 	for i in range(len(data_list)):
 		li.append([data_list[i], i])
-	  
+	
 	li.sort(reverse=True)
 	sort_index = []
 	for x in li:
@@ -178,20 +184,11 @@ def sort_list_reverse(data_list):
 	
 	return data_argsort_i
 		
-
-def compute_cri(rank1,rank2,rank3,rank4):
-	
-	p1 = (rank1+rank2+rank3+rank4)
-	p2 = p1/68
-	cri = 1 - p2
-	
-	return cri
-	
 	
 # Import cmip models and obs database 
-area_cmip6 = 'namz'
-var_obs = 'pr'
-var_cmip6 = 'pr'
+area_cmip6 = 'br'
+var_obs = 'Tmin'
+var_cmip6 = 'tasmin'
 dt = '1986-2005'
 
 namz_obs_latlon = import_obs_latlon(var_obs, 'NAMZ', 'ANN', dt)
@@ -280,83 +277,83 @@ for i in range(1, 18):
 	rmse_namz_cmip6.append(compute_rmse(namz_cmip_latlon, namz_obs_latlon))	
 	tss_namz_cmip6.append(compute_tss(namz_obs_latlon, namz_cmip_latlon))
 	pcc_namz_cmip6.append(compute_pcc(namz_obs_mon_ts, namz_cmip_mon_ts))
-	ivs_namz_cmip = compute_ivs(namz_obs_ann_ts, namz_cmip_ann_ts)
-	ivs_namz_cmip6.append(np.nanmean(ivs_namz_cmip))
+	ivs_namz_cmip6.append(compute_ivs(namz_obs_ann_ts, namz_cmip_ann_ts))
 	
 	mbe_samz_cmip6.append(compute_mbe(samz_cmip_latlon, samz_obs_latlon))
 	rmse_samz_cmip6.append(compute_rmse(samz_cmip_latlon, samz_obs_latlon))
 	tss_samz_cmip6.append(compute_tss(samz_obs_latlon, samz_cmip_latlon))
 	pcc_samz_cmip6.append(compute_pcc(samz_obs_mon_ts, samz_cmip_mon_ts))
-	ivs_samz_cmip = compute_ivs(samz_obs_ann_ts, samz_cmip_ann_ts)
-	ivs_samz_cmip6.append(np.nanmean(ivs_samz_cmip))
+	ivs_samz_cmip6.append(compute_ivs(samz_obs_ann_ts, samz_cmip_ann_ts))
 		
 	mbe_neb_cmip6.append(compute_mbe(neb_cmip_latlon, neb_obs_latlon))
 	rmse_neb_cmip6.append(compute_rmse(neb_cmip_latlon, neb_obs_latlon))
 	tss_neb_cmip6.append(compute_tss(neb_obs_latlon, neb_cmip_latlon))
 	pcc_neb_cmip6.append(compute_pcc(neb_obs_mon_ts, neb_cmip_mon_ts))
-	ivs_neb_cmip = compute_ivs(neb_obs_ann_ts, neb_cmip_ann_ts)
-	ivs_neb_cmip6.append(np.nanmean(ivs_neb_cmip))
+	ivs_neb_cmip6.append(compute_ivs(neb_obs_ann_ts, neb_cmip_ann_ts))
 		
 	mbe_sam_cmip6.append(compute_mbe(sam_cmip_latlon, sam_obs_latlon))
 	rmse_sam_cmip6.append(compute_rmse(sam_cmip_latlon, sam_obs_latlon))
 	tss_sam_cmip6.append(compute_tss(sam_obs_latlon, sam_cmip_latlon))
 	pcc_sam_cmip6.append(compute_pcc(sam_obs_mon_ts, sam_cmip_mon_ts))
-	ivs_sam_cmip = compute_ivs(sam_obs_ann_ts, sam_cmip_ann_ts)
-	ivs_sam_cmip6.append(np.nanmean(ivs_sam_cmip))
+	ivs_sam_cmip6.append(compute_ivs(sam_obs_ann_ts, sam_cmip_ann_ts))
 		
 	mbe_lpb_cmip6.append(compute_mbe(lpb_cmip_latlon, lpb_obs_latlon))
 	rmse_lpb_cmip6.append(compute_rmse(lpb_cmip_latlon, lpb_obs_latlon))
 	tss_lpb_cmip6.append(compute_tss(lpb_obs_latlon, lpb_cmip_latlon))
 	pcc_lpb_cmip6.append(compute_pcc(lpb_obs_mon_ts, lpb_cmip_mon_ts))
-	ivs_lpb_cmip = compute_ivs(lpb_obs_ann_ts, lpb_cmip_ann_ts)
-	ivs_lpb_cmip6.append(np.nanmean(ivs_lpb_cmip))
+	ivs_lpb_cmip6.append(compute_ivs(lpb_obs_ann_ts, lpb_cmip_ann_ts))
 	
 	mbe_br_cmip6.append(compute_mbe(br_cmip_latlon, br_obs_latlon))
 	rmse_br_cmip6.append(compute_rmse(br_cmip_latlon, br_obs_latlon))
 	tss_br_cmip6.append(compute_tss(br_obs_latlon, br_cmip_latlon))
 	pcc_br_cmip6.append(compute_pcc(br_obs_mon_ts, br_cmip_mon_ts))
-	ivs_br_cmip = compute_ivs(br_obs_ann_ts, br_cmip_ann_ts)
-	ivs_br_cmip6.append(np.nanmean(ivs_br_cmip))
+	ivs_br_cmip6.append(compute_ivs(br_obs_ann_ts, br_cmip_ann_ts))
 
 	legend.append(cmip6[i][0])
 
 if area_cmip6 == 'namz':
+	argsort_mbe = sort_list(mbe_namz_cmip6)
 	argsort_rmse = sort_list(rmse_namz_cmip6)
 	argsort_tss = sort_list_reverse(tss_namz_cmip6)
 	argsort_pcc = sort_list_reverse(pcc_namz_cmip6)
 	argsort_ivs = sort_list(ivs_namz_cmip6)
 elif area_cmip6 == 'samz':
+	argsort_mbe = sort_list(mbe_samz_cmip6)
 	argsort_rmse = sort_list(rmse_samz_cmip6)
 	argsort_tss = sort_list_reverse(tss_samz_cmip6)
 	argsort_pcc = sort_list_reverse(pcc_samz_cmip6)
 	argsort_ivs = sort_list(ivs_samz_cmip6)
 elif area_cmip6 == 'neb':
+	argsort_mbe = sort_list(mbe_neb_cmip6)
 	argsort_rmse = sort_list(rmse_neb_cmip6)
 	argsort_tss = sort_list_reverse(tss_neb_cmip6)
 	argsort_pcc = sort_list_reverse(pcc_neb_cmip6)
 	argsort_ivs = sort_list(ivs_neb_cmip6)
 elif area_cmip6 == 'sam':
+	argsort_mbe = sort_list(mbe_sam_cmip6)
 	argsort_rmse = sort_list(rmse_sam_cmip6)
 	argsort_tss = sort_list_reverse(tss_sam_cmip6)
 	argsort_pcc = sort_list_reverse(pcc_sam_cmip6)
 	argsort_ivs = sort_list(ivs_sam_cmip6)
 elif area_cmip6 == 'lpb':
+	argsort_mbe = sort_list(mbe_lpb_cmip6)
 	argsort_rmse = sort_list(rmse_lpb_cmip6)
 	argsort_tss = sort_list_reverse(tss_lpb_cmip6)
 	argsort_pcc = sort_list_reverse(pcc_lpb_cmip6)
 	argsort_ivs = sort_list(ivs_lpb_cmip6)
 else:
+	argsort_mbe = sort_list(mbe_br_cmip6)
 	argsort_rmse = sort_list(rmse_br_cmip6)
 	argsort_tss = sort_list_reverse(tss_br_cmip6)
 	argsort_pcc = sort_list_reverse(pcc_br_cmip6)
 	argsort_ivs = sort_list(ivs_br_cmip6)
 
-print()
+print('mbe_{0}_{1} ='.format(area_cmip6, var_cmip6), argsort_mbe)
 print('rmse_{0}_{1} ='.format(area_cmip6, var_cmip6), argsort_rmse)
 print('tss_{0}_{1} ='.format(area_cmip6, var_cmip6), argsort_tss)
 print('pcc_{0}_{1} ='.format(area_cmip6, var_cmip6), argsort_pcc)
 print('ivs_{0}_{1} ='.format(area_cmip6, var_cmip6), argsort_ivs)
-print()
+
 
 
 
