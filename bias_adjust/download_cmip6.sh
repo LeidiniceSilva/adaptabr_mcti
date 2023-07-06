b7e36ee404691d478f70183b9e8bdc7de76967fb
Leidinice Silva <leidinicesilva@gmail.com>
2023-07-06 18:31:41 -0300

    update script

--- bias_adjust/download_cmip6.sh
+++ bias_adjust/download_cmip6.sh
@@ -2,29 +2,62 @@
 
 #__author__      = 'Leidinice Silva'
 #__email__       = 'leidinicesilva@gmail.com'
-#__date__        = '07/05/2023'
+#__date__        = 'Jul 05, 2023'
 #__description__ = 'Download CMIP6 simulations and projections'
 
-exp='ssp245'
+# center
+center=( 'DKRZ' )
 
-PATH="/media/nice/Nice/documentos/projetos/AdaptaBrasil_MCTI/database/bias_adjust/cmip6/GFDL-ESM4/"${exp}    	    
-cd ${PATH}
+# model 
+model=( 'MPI-ESM1-2-HR' )
 
+# Variable list
+var_list=( 'pr' 'tasmax' 'tasmin' )
 
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/pr/gr1/files/d20180701/pr_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20150101-20341231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/pr/gr1/files/d20180701/pr_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20350101-20541231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/pr/gr1/files/d20180701/pr_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20550101-20741231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/pr/gr1/files/d20180701/pr_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20750101-20941231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/pr/gr1/files/d20180701/pr_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20950101-21001231.nc
+#~ for var in ${var_list[@]}; do
 
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmax/gr1/files/d20180701/tasmax_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20150101-20341231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmax/gr1/files/d20180701/tasmax_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20350101-20541231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmax/gr1/files/d20180701/tasmax_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20550101-20741231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmax/gr1/files/d20180701/tasmax_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20750101-20941231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmax/gr1/files/d20180701/tasmax_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20950101-21001231.nc
+	#~ # Path to save file
+	#~ PATH="/media/nice/Nice/documentos/projetos/AdaptaBrasil_MCTI/database/bias_adjust/cmip6/${model}/historical"    	    
+	#~ cd ${PATH}
 
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmin/gr1/files/d20180701/tasmin_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20150101-20341231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmin/gr1/files/d20180701/tasmin_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20350101-20541231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmin/gr1/files/d20180701/tasmin_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20550101-20741231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmin/gr1/files/d20180701/tasmin_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20750101-20941231.nc	
-/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/NOAA-GFDL/GFDL-ESM4/${exp}/r1i1p1f1/day/tasmin/gr1/files/d20180701/tasmin_day_GFDL-ESM4_${exp}_r1i1p1f1_gr1_20950101-21001231.nc
+	#~ # Download file
+	#~ /usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/CMIP/${center}/${model}/historical/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_historical_r1i1p1f1_gn_19850101-19891231.nc			
+	#~ /usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/CMIP/${center}/${model}/historical/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_historical_r1i1p1f1_gn_19900101-19941231.nc		
+	#~ /usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/CMIP/${center}/${model}/historical/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_historical_r1i1p1f1_gn_19950101-19991231.nc		
+	#~ /usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/CMIP/${center}/${model}/historical/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_historical_r1i1p1f1_gn_20000101-20041231.nc		
+	#~ /usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/CMIP/${center}/${model}/historical/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_historical_r1i1p1f1_gn_20050101-20091231.nc
+
+#~ done
+
+# scenario list
+scenario_list=( 'ssp126' 'ssp245' 'ssp585' )
+
+for exp in ${scenario_list[@]}; do
+	for var in ${var_list[@]}; do
+
+		# Path to save file
+		PATH="/media/nice/Nice/documentos/projetos/AdaptaBrasil_MCTI/database/bias_adjust/cmip6/${model}/${exp}"    	    
+		cd ${PATH}
+
+		# Download file
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20150101-20191231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20200101-20241231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20250101-20291231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20300101-20341231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20350101-20391231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20400101-20441231.nc		
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20450101-20491231.nc		
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20500101-20541231.nc		
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20550101-20591231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20600101-20641231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20650101-20691231.nc		
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20700101-20741231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20750101-20791231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20800101-20841231.nc		
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20850101-20891231.nc
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20900101-20941231.nc			
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_20950101-20991231.nc		
+		/usr/bin/wget wget -N -c https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/${center}/${model}/${exp}/r1i1p1f1/day/${var}/gn/files/d20190710/${var}_day_${model}_${exp}_r1i1p1f1_gn_21000101-21001231.nc
+		  
+	done
+done
