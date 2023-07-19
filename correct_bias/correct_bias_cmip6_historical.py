b94459165b6e693a18fb8461aa3b7a476fb72d06
Leidinice Silva <leidinicesilva@gmail.com>
2023-07-19 20:23:10 -0300

    update scripts

--- correct_bias/correct_bias_cmip6_historical.py
+++ correct_bias/correct_bias_cmip6_historical.py
@@ -7,14 +7,20 @@ __description__ = "This script correct bias of cmip6 models"
 
 import os
 import netCDF4
+import calendar
 import numpy as np
 import pandas as pd
 
 from netCDF4 import Dataset
+from scipy.stats import gamma
 from dict_cmip6_models_name import cmip6
 
-dataset_dir = "/media/nice/Nice/documentos/projetos/AdaptaBrasil_MCTI/database/correct_bias"
+# Dataset directory
+dataset_dir = "/home/nice/Documentos/AdaptaBrasil_MCTI/database/correct_bias"
 
+# Best models list
+best_models = [17, 7, 13, 9, 15]
+i = 7
 
 def import_observed(var_name, target_date):
 	
@@ -57,18 +63,19 @@ def import_simulated(model_name, exp_name, var_name, member, target_date):
 	return lat, lon, value 
     
 
+
 def correct_bias(simulated_data, observed_data):
-	
-    # Get the shape of the simulated data array
-    shape = simulated_data.shape
-    
-    # Correct each grid point separately
-    corrected_data = np.zeros_like(simulated_data)
-    for i in range(shape[1]):
-        for j in range(shape[2]):
-            corrected_data[:, i, j] = correct_bias_1d(simulated_data[:, i, j], observed_data[:, i, j])
-    
-    return corrected_data
+
+	# Get the shape of the simulated data array
+	shape = simulated_data.shape
+
+	# Correct each grid point separately
+	corrected_data = np.zeros_like(simulated_data)
+	for i in range(shape[1]):
+		for j in range(shape[2]):
+			corrected_data[:, i, j] = correct_bias_1d(simulated_data[:, i, j], observed_data[:, i, j])
+
+	return corrected_data
     
 
 def correct_bias_1d(simulated_values, observed_values):
@@ -85,100 +92,158 @@ def correct_bias_1d(simulated_values, observed_values):
     
     return corrected_values
     
-   
+
+def get_leap_day_indices(start_year, end_year):
+	
+    leap_day_indices = []
+    for year in range(start_year, end_year + 1):
+        if calendar.isleap(year):
+            # Leap day (February 29) corresponds to index 59 in a non-leap year and index 60 in a leap year
+            leap_day_indices.append((year - start_year) * 365 + 59 if calendar.monthrange(year, 2)[1] == 29 else (year - start_year) * 365 + 60)
+    
+    return leap_day_indices
+
+
+def remove_leap_days(array, indices):
+	
+    # Create a mask to identify the indices to delete
+    mask = np.ones(array.shape[0], dtype=bool)
+    mask[indices] = False
+
+    # Use the mask to select the non-deleted indices
+    new_array = array[mask]
+
+    return new_array
+    
+    
 def write_3d_nc(ncname, var_array, time_array, lat_array, lon_array, var_units,
                 var_shortname, var_longname, time_units, missing_value=-999.):
-					
-    """Write 3 dimensional (time, lat, lon) netCDF4
-    :param ncname: Name of the output netCDF
-    :param var_array: 3D array (time, lat, lon)
-    :param time_array: 1D array with unique value
-    :param lat_array: 1D array which contains the lat values
-    :param lon_array: 1D array which contains the lon values
-    :param var_units: variable units
-    :param var_shortname: variable short name
-    :param var_longname: variable long name
-    :param time_units: time units
-    :param missing_value: float missing value
-    """
+	
+	"""Write 3 dimensional (time, lat, lon) netCDF4
+	:param ncname: Name of the output netCDF
+	:param var_array: 3D array (time, lat, lon)
+	:param time_array: 1D array with unique value
+	:param lat_array: 1D array which contains the lat values
+	:param lon_array: 1D array which contains the lon values
+	:param var_units: variable units
+	:param var_shortname: variable short name
+	:param var_longname: variable long name
+	:param time_units: time units
+	:param missing_value: float missing value
+	"""
 
-    foo = Dataset(ncname, 'w', format='NETCDF4_CLASSIC')
+	foo = Dataset(ncname, 'w', format='NETCDF4_CLASSIC')
 
-    foo.createDimension('time', None)
-    foo.createDimension('lat', lat_array.shape[0])
-    foo.createDimension('lon', lon_array.shape[0])
+	foo.Conventions = 'CF-1.7 CMIP-6.0 UGRID-1.0'
+	foo.title 		= 'GFDL-ESM4 correct bias model output'
+	foo.institution = 'NOAA GFDL, Princeton, NJ 08540, USA'
+	foo.source 		= 'GFDL-ESM4'
+	foo.history 	= 'CMIP6 model with corrected bias'
+	foo.references 	= 'https://esgf.ceda.ac.uk/thredds/catalog/esg_cmip6/catalog.html'
+	foo.comment 	= 'This netCDF4 file was corrected using GQM function'
 
-    times = foo.createVariable('time', float, ('time'))
-    times.units = time_units
-    times.calendar = 'standard'
-    times[:] = range(len(time_array))
+	foo.createDimension('time', None)
+	foo.createDimension('lat', lat_array.shape[0])
+	foo.createDimension('lon', lon_array.shape[0])
 
-    laty = foo.createVariable('lat', 'f4', 'lat')
-    laty.units = 'degrees_north'
-    laty.long_name = 'latitude'
-    laty[:] = lat_array
+	times = foo.createVariable('time', float, ('time'))
+	times.units = time_units
+	times.calendar = 'standard'
+	times[:] = range(len(time_array))
 
-    lonx = foo.createVariable('lon', 'f4', 'lon')
-    lonx.units = 'degrees_east'
-    lonx.long_name = 'longitude'
-    lonx[:] = lon_array
+	laty = foo.createVariable('lat', 'f4', 'lat')
+	laty.units = 'degrees_north'
+	laty.long_name = 'latitude'
+	laty[:] = lat_array
 
-    var = foo.createVariable(var_shortname, float, ('time', 'lat', 'lon'))
-    var.units = var_units
-    var.long_name = var_longname
-    var.missing_value = missing_value
-    var[:] = var_array
+	lonx = foo.createVariable('lon', 'f4', 'lon')
+	lonx.units = 'degrees_east'
+	lonx.long_name = 'longitude'
+	lonx[:] = lon_array
 
-    foo.close()
+	var = foo.createVariable(var_shortname, float, ('time', 'lat', 'lon'))
+	var.units = var_units
+	var.long_name = var_longname
+	var.missing_value = missing_value
+	var[:] = var_array
+
+	foo.close()
 
 
-# Import cmip models and obs database 
-best_models = [17, 7, 13, 9, 15]
-i = 9
-
-# create date list
-time_array = pd.date_range('1986-01-01','1986-12-31', freq='D').strftime('%Y-%m-%d').tolist()
+# Create date list
+time_array = pd.date_range('1986-01-01','2005-12-31', freq='D').strftime('%Y-%m-%d').tolist()
+time_list = pd.date_range('1986','2006', freq='Y').strftime('%Y').tolist()
 
 dt = '19860101-20051231'
 experiment = 'historical'
-var_obs = 'Tmin'
-var_cmip6 = 'tasmin'
+var_obs = 'pr'
+var_cmip6 = 'pr'
 
 print(cmip6[i][0])
 print(experiment)
 print(var_cmip6)
 
-lat, lon, observed = import_observed(var_obs, dt)
+# Import cmip models and obs database 
+lat, lon, obs = import_observed(var_obs, dt)
 lat_array, lon_array, simulated = import_simulated(cmip6[i][0], experiment, var_cmip6, cmip6[i][1], dt)
 
-# Import correct bias function
-corrected_dataset = correct_bias(simulated[0:365, :, :], observed[0:365, :, :])
+print('first step')
+print(obs.shape)
+print(simulated.shape)
 
-# Print the shape of the corrected array
-print(corrected_dataset.shape)
+# leap day indices to delete
+leap_day_indices = get_leap_day_indices(1986, 2005)
 
-if var_cmip6 == 'pr':
-	var_units = 'mm'
-	var_shortname = 'pr'
-	var_longname = 'Daily Total Precipitation'
-	time_units = 'days since {}'.format(time_array[0])
-elif var_cmip6 == 'tasmax':
-	var_units = 'Celcius degrees'
-	var_shortname = 'tasmax'
-	var_longname = 'Daily Maximum Near-Surface Air Temperature'
-	time_units = 'days since {}'.format(time_array[0])
+# Remove leap day (Feb 29th) from observed dataset
+if i == 7:
+	observed = remove_leap_days(obs, leap_day_indices)
+elif i == 9:
+	observed = remove_leap_days(obs, leap_day_indices)
 else:
-	var_units = 'Celcius degrees'
-	var_shortname = 'tasmin'
-	var_longname = 'Daily Minimum Near-Surface Air Temperature'
-	time_units = 'days since {}'.format(time_array[0])
+	observed = obs
+
+print('second step')
+print(observed.shape)
+print(simulated.shape)
+
+sample_list = list(range(0, 7300))
+def group_elements(lst, group_size=365):
+    grouped_list = [lst[i:i + group_size] for i in range(0, len(lst), group_size)]
+    return grouped_list
+
+# Call the function with the sample_list and group size 5
+grouped_elements = group_elements(sample_list, group_size=365)
+
+print('third step')
+# Print the grouped elements
+for idx, gp in enumerate(grouped_elements):
+	print(time_list[idx], gp[0], gp[-1])
+
+	# Import correct bias function
+	corrected_dataset = correct_bias(simulated[gp[0]:gp[-1], :,:], observedsimulated[gp[0]:gp[-1])
+
+	# Print the shape of the corrected array
+	print(corrected_dataset.shape)
+
+	if var_cmip6 == 'pr':
+		var_units = 'mm'
+		var_shortname = 'pr'
+		var_longname = 'Daily Total Precipitation'
+		time_units = 'days since {}'.format(time_array[0])
+	elif var_cmip6 == 'tasmax':
+		var_units = 'Celcius degrees'
+		var_shortname = 'tasmax'
+		var_longname = 'Daily Maximum Near-Surface Air Temperature'
+		time_units = 'days since {}'.format(time_array[0])
+	else:
+		var_units = 'Celcius degrees'
+		var_shortname = 'tasmin'
+		var_longname = 'Daily Minimum Near-Surface Air Temperature'
+		time_units = 'days since {}'.format(time_array[0])
 	
-# Path out to save netCDF4
-nc_output = '{0}/cmip6_correct/{1}/{2}/{3}_br_day_{1}_{2}_{4}_{5}_correct.nc'.format(dataset_dir, cmip6[i][0], experiment, var_cmip6, cmip6[i][1], dt)
-write_3d_nc(nc_output, corrected_dataset, time_array, lat_array, lon_array, var_units, var_shortname, var_longname, time_units, missing_value=-999.)
-exit()
-
-
-
+	print('fourth step')	
+	# Path out to save netCDF4
+	nc_output = '{0}/cmip6_correct/{1}/{2}/{3}_br_day_{1}_{2}_{4}_{5}_correct.nc'.format(dataset_dir, cmip6[i][0], experiment, var_cmip6, cmip6[i][1], time_list[idx])
+	write_3d_nc(nc_output, corrected_dataset, time_array, lat_array, lon_array, var_units, var_shortname, var_longname, time_units, missing_value=-999.)
 
 
